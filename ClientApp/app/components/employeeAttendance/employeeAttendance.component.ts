eefd41b69b1f9a2e1f498097320d8b7a5415971b
VivekVWaghmare <wvvivek1790@gmail.com>
2018-02-14 02:13:40 +0530

    tried some css for Attendance page

--- ClientApp/app/components/employeeAttendance/employeeAttendance.component.ts
+++ ClientApp/app/components/employeeAttendance/employeeAttendance.component.ts
@@ -12,13 +12,13 @@ import { EmployeeService } from '../../common/services/empservice.service';
 
 export class EmpTodayAttendanceComponent implements OnInit{
     ngOnInit(): void {
-        this._employeeService.getEmployees().subscribe(
-            data => this.empData= data  
+        this.employeeService.getEmployees().subscribe(
+            data => this.empData= data
         ) 
     }
     public empData: EmployeeData[] = [];
 
-    constructor(private http: Http, private router: Router, private _employeeService: EmployeeService){
+    constructor(private http: Http, private router: Router, private employeeService: EmployeeService){
         
     }
 
